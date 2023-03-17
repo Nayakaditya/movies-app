a9945b11dabb00ebf1e6b3b211b1e3a7dab7f413
Aditya <adi7961n@gmail.com>
2023-03-17 13:36:00 +0530

    redux store created at index.js file in the src root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./components/App";
+import movies from "./redux/reducers";
+
+// creating a reducer here which container reducers as an object
+const reducer = {
+  movies,
+};
+
+// redux store
+const store = configureStore({
+  reducer,
+});
+
+console.log("store ", store);
+console.log("State ", store.getState());
+// console.log("STATE ", store.getState());
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
